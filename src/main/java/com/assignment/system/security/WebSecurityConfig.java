8b804ee460ee3eb4d69109728d91241181b15259
kalamazaad <abdulkalamazaad2447@gmail.com>
2026-03-25 15:22:48 +0530

    fix cors for render

--- src/main/java/com/assignment/system/security/WebSecurityConfig.java
+++ src/main/java/com/assignment/system/security/WebSecurityConfig.java
@@ -70,7 +70,7 @@ public class WebSecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173", "http://localhost:3000")); // Vite and
+        configuration.setAllowedOrigins(Arrays.asList("https://assignflow-gvc1.onrender.com")); // Vite and
                                                                                                           // CRA default
                                                                                                           // ports
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
